7b89d658d49fae84e17a327739eb516e2b357d60
Daniel51M0N <106700464+Daniel51M0N@users.noreply.github.com>
2022-06-01 17:56:08 -0400

    Update MontyHallProblem.java

--- MontyHallProblem.java
+++ MontyHallProblem.java
@@ -1,4 +1,4 @@
-public class MontyHallProblem {
+class MontyHallProblem {
   public static void main(String[] args) {
     int numDoors = 100;
     int repetitions = 100;
@@ -7,29 +7,28 @@ public class MontyHallProblem {
     for (int i = repetitions; i > 0; i--) {
       int correctDoor = (int) (Math.random() * numDoors);
 
-      // System.out.println("The random correct door has now been chosen out of the "
-      // + numDoors + " options. It is door number " + correctDoor + ".\n");
+      //System.out.println("The random correct door has now been chosen out of the " + numDoors + " options. It is door number " + correctDoor + ".\n");
 
       int chosenDoor = (int) (Math.random() * numDoors);
 
-      // System.out.println("Now, " + (numDoors - 1) + " doors have been removed from
-      // play. The remaining doors are:\n");
+      //System.out.println("Now, " + (numDoors - 1) + " doors have been removed from play. The remaining doors are:");
 
       if (chosenDoor == correctDoor) {
         int otherDoor;
         do {
           otherDoor = (int) (Math.random() * numDoors);
         } while (otherDoor != chosenDoor);
+
         //System.out.println("Chosen door: #" + chosenDoor + " (correct)\n" + "Other door: #" + otherDoor + " (incorrect)");
-        //System.out.println("They should NOT have switched doors!");
+        System.out.println("They should NOT have switched doors!");
       } else {
         //System.out.println("Chosen door: #" + chosenDoor + " (incorrect)\n" + "Other door: #" + correctDoor + " (correct)");
-        //System.out.println("They SHOULD have switched doors!");
-        wouldHaveWon ++;
+        System.out.println("They SHOULD have switched doors!");
+        wouldHaveWon++;
       }
-      System.out.println("\n");
     }
-    
+
     System.out.println("The contestant would have won upon switching doors " + wouldHaveWon + " out of " + repetitions + " times!");
+    System.out.println("\n\n");
   }
 }
